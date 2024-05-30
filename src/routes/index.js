a94ba2389620a4be0269e43b4f9969c6e44bb2ec
sonnguyen16 <sonnv2212@gmail.com>
2024-05-30 15:01:45 +0700

    #1 - add middleware for check api key

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,8 +1,10 @@
 import { Router } from "express"
 import authRouter from "./auth/index.js"
+import { checkApiKey } from "../middlewares/checkapikey.middleware.js"
 
 const router = Router()
 
+router.use(checkApiKey)
 router.use('/', authRouter)
 
 export default router
